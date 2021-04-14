d516d0f799aa90da4e2cf6839567db960fad1766
Andraz Krasovec <ak6688@student.uni-lj.si>
2021-04-14 11:06:37 +0200

    Modified to run on full sample.

--- network.py
+++ network.py
@@ -6,9 +6,9 @@ from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 class Network:
 
-    def __init__(self, image_shape=(256, 256, 3)):
+    def __init__(self, image_shape=(256, 256, 3), class_num=50):
         self.image_shape = image_shape
-        pass
+        self.class_num = class_num
 
     def generate_model(self):
         """
@@ -17,12 +17,14 @@ class Network:
 
         in_layer = Input(shape=self.image_shape)
         x = Conv2D(32, kernel_size=4, activation="relu")(in_layer)
-        x = MaxPooling2D(pool_size=(4, 4))(x)
-        x = Conv2D(32, kernel_size=4, activation="relu")(x)
-        x = MaxPooling2D(pool_size=(4, 4))(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Conv2D(64, kernel_size=4, activation="relu")(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Conv2D(128, kernel_size=4, activation="relu")(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
         x = Flatten()(x)
-        x = Dense(8, activation="relu")(x)
-        out_layer = Dense(5, activation="softmax")(x)
+        # x = Dense(50, activation="relu")(x)
+        out_layer = Dense(self.class_num, activation="softmax")(x)
 
         return Model(inputs=in_layer, outputs=out_layer)
 
